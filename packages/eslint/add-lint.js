3c7310338cb3945b29c682ddfbe6e965b0f3471f
Drunken_J(Hoon.shin) <mycoolade@gmail.com>
2020-01-09 16:39:47 +0900

    eslint package 배포 준비
    namespace를 glacier에서 eslint로 변경

--- packages/eslint/add-lint.js
+++ packages/eslint/add-lint.js
@@ -7,42 +7,42 @@ const fs = require('fs')
 const chunk = fs.readFileSync(packageFile, 'utf-8')
 const pack = JSON.parse(chunk)
 
-const glacier = 'glacier-eslint'
+const drunken = 'drunken-eslint'
 
 
 if (pack.eslintConfig) {
   if (pack.eslintConfig.extends) {
     if (typeof pack.eslintConfig.extends === 'string') {
-      if (pack.eslintConfig.extends === glacier) { return }
+      if (pack.eslintConfig.extends === drunken) { return }
       const result = [
         pack.eslintConfig.extends,
-        glacier,
+        drunken,
       ]
 
       pack.eslintConfig.extends = result
     } else {
       let isExist;
       pack.eslintConfig.extends.forEach((text) => {
-        if (text === glacier) {
+        if (text === drunken) {
           isExist = true
           return isExist
         }
       })
       if (isExist) { return }
-      pack.eslintConfig.extends.push(glacier)
+      pack.eslintConfig.extends.push(drunken)
     }
   } else {
     console.log(pack.eslintConfig)
-    pack.eslintConfig.extends = glacier
+    pack.eslintConfig.extends = drunken
   }
 } else {
   pack.eslintConfig = {}
-  pack.eslintConfig.extends = glacier
+  pack.eslintConfig.extends = drunken
 }
 
 const data = JSON.stringify(pack, null, 2)
 
 fs.writeFileSync(packageFile, data, 'utf-8', (err) => {
-  console.log('Set glacier-eslint rule in your package.json...'.blue)
+  console.log('Set drunken-eslint rule in your package.json...'.blue)
   err ? console.log('something is wrong!'.red) : console.log('done'.blue)
 })
